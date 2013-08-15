8b91b3659a5895b0781f6017bf9919c30fe05119
biluo1989 <biluo.han@gmail.com>
2013-08-15 10:45:27 +0800

    Initial Commit

---
+++ StoreSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by APPLE on 13-8-15.
+//  Copyright (c) 2013年 Yujiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StoreSearch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StoreSearch
+//
+//  Created by APPLE on 13-8-15.
+//  Copyright (c) 2013年 Yujiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
